7c58ffaddd02ffc74257740f48ac1eb44d5516b9
Matt Gordon <bigmattsize21@gmail.com>
2020-03-20 03:23:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,31 +1,20 @@
 class School
+ attr_accessor :name, :roster
   
-  attr_accessor :roster,
-  def initialize(name)
-    @name=name
-    @roster={}
-  end
+   def initialize(name)
+    @name = name
+     @roster = {}
+   end 
   
-end
-class School
-#   attr_accessor :name, :roster
+   def add_student(student, level)
+    roster[level] ||= []
+   roster[level] << student
+  end 
   
-#   def initialize(name)
-#     @name = name
-#     @roster = {}
-#   end 
-  
-#   def add_student(student, level)
-#     # this sort of means #=> a || a = b ... or #=> x || x = y
-#     # it's freak'n weird
-#     roster[level] ||= []
-#     roster[level] << student
-#   end 
-  
-#   def grade(level)
-#     roster.detect do |x, y| 
-#       if x == level
-#         return y 
-#       end 
-#     end 
-#   end 
+   def grade(level)
+     roster.detect do |x, y| 
+    if x == level
+        return y 
+      end 
+    end 
+   end 
